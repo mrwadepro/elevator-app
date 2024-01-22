21412be9ba2b676c0130085fb41e117fb85e2f9f
mrwadepro <mrwadepro@gmail.com>
2024-01-21 22:29:48 -0600

    add timeout again

--- src/pages/api/createMessage.ts
+++ src/pages/api/createMessage.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export const maxDuration = 300000 // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic'
 
 export default async function createMessage(req: NextApiRequest, res: NextApiResponse) {
   const { messages } = req.body
