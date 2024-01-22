a7d47e0b94d24790df97284df50d9ffbb664938d
mrwadepro <mrwadepro@gmail.com>
2024-01-21 20:34:41 -0600

    add timeout

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -2,15 +2,19 @@ import { ChatCompletionRequestMessage } from 'openai'
 
 export const sendMessage = async (messages: ChatCompletionRequestMessage[]) => {
   try {
+    const controller = new AbortController()
+
+    const timeoutId = setTimeout(() => controller.abort(), 300000)
+
     const response = await fetch('/api/createMessage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ messages }),
-      timeout: 300000
+      signal: controller.signal
     })
-
+    clearTimeout(timeoutId)
     return await response.json()
   } catch (error) {
     console.log(error)
